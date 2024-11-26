fc625324f4a2678a798aa48c64b5c39cec9b2bf1
Rick <111731365+cleavethis@users.noreply.github.com>
2024-11-25 22:45:08 -0800

    add Delete endpoint

--- FinShark/Controllers/StockController.cs
+++ FinShark/Controllers/StockController.cs
@@ -70,6 +70,22 @@ namespace FinShark.Controllers
             _context.SaveChanges();
             return Ok(stockModel.ToStockDto());
         }
+
+        [HttpDelete]
+        [Route("{id}")]
+
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stock.FirstOrDefault(x => x.stockId == id);
+
+            if (stockModel == null) { return NotFound(); };
+
+            _context.Stock.Remove(stockModel);
+            _context.SaveChanges();
+
+            return NoContent();
+
+        }
     }
            
 }
